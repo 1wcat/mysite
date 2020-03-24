9663ab0364affd7aaf61e62e305044d654c3cc2a
Lilly-Anne Wei <lillyannewei@yahoo.com>
2020-03-24 19:49:41 +0800

    HW

--- 20200320python/images.py
+++ 20200320python/images.py
@@ -6,10 +6,12 @@ def getHtml():
     return s
 
 def main():
-    item_list = [
-        { 'title': 'mountain1', 'url': "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTEVahhGSoA4A4OIwF-0BPv0S4VOcJlW6XzdOx6sE6f7p_-ZOK9"},
+    image_list = [
+        { 'title': 'picture1', 'url': "https://i.pinimg.com/736x/11/2b/74/112b746a2182417b2a947d949798c968.jpg"},
+        { 'title': 'picture2', 'url': "https://www.serebii.net/swordshield/pokemon/778.png"},
+        { 'title': 'picture3', 'url': "https://cdn.shopify.com/s/files/1/0217/9998/products/african-gyename-mask.jpg?v=1492402709"}
     ]
-    
+      
     tmpl = Template(getHtml())
     print(tmpl.render({'image': images_list}))
 
